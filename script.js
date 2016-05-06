cbe8f81ecafdbc6d9927c1bbce75601a7bc9266d
Alex <korotkov2005@gmail.com>
2016-05-06 15:05:35 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        i: ['block1', 'block2', 'block3'],
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 1000
+    });
+  $('.text-center').css("color", "blue");
+});
